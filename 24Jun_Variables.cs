b01a25b425ceeeb38d1ca01df93267f3668c49f6
srikanthpayavula <86397772+srikanthpayavula@users.noreply.github.com>
2021-06-24 20:18:27 +0530

    Add files via upload

---
+++ 24Jun_Variables.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace onedrive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+    int a=10,b=20;
+      short c=30;
+      double k=a+b+c; 
+      Console.WriteLine("Result : " + k);
+         a=Convert.ToInt32(Console.ReadLine());
+           k=a+b+c;
+           //  Console.WriteLine("after adding input of a={0}",a);
+             Console.WriteLine("Result after adding input of a : " + k);
+        }
+    }
+}
