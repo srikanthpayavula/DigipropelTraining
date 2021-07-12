a9ab05a457261c5eced51f84fb4a770234ef738d
srikanthpayavula <86397772+srikanthpayavula@users.noreply.github.com>
2021-07-12 05:49:49 +0530

    Update 07Jul_09.cs

--- 07Jul_09.cs
+++ 07Jul_09.cs
@@ -10,43 +10,43 @@ namespace July
 {
     class _07Jul_09
     {
-    //    static void Main(string[] args)
-    //    {
-    //        String line;
-    //        try
-    //        {
-    //            ArrayList words = new ArrayList();
-    //            //Pass the file path and file name to the StreamReader constructor
-    //            StreamReader sr = new StreamReader("E:\\practice\\Sample.txt");
-    //            int count = 0;
-    //            //Read the first line of text
-    //            line = sr.ReadLine();
-    //            //Continue to read until you reach end of file
-    //            while (line != null)
-    //            {
-    //                //write the lie to console window
-    //                String[] string1 = line.ToLower().Split(new Char[] { ',', '.', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        static void Main(string[] args)
+        {
+            String line;
+            try
+            {
+                ArrayList words = new ArrayList();
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("E:\\practice\\Sample.txt");
+                int count = 0;
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the lie to console window
+                   String[] string1 = line.ToLower().Split(new Char[] { ',', '.', ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
-    //                Console.WriteLine(line);
-    //                foreach (String s in string1)
-    //                {
-    //                    words.Add(s);
-    //                }
-    //                count++;
+                    Console.WriteLine(line);
+                    foreach (String s in string1)
+                    {
+                        words.Add(s);
+                    }
+                    count++;
 
-    //                //Read the next line
-    //                line = sr.ReadLine();
-    //            }
-    //            // Console.WriteLine("total count of lines  " + count);
-    //            //close the file
-    //            sr.Close();
-    //            Console.ReadLine();
-    //        }
-    //        catch (Exception e)
-    //        {
-    //            Console.WriteLine("Exception: " + e.Message);
-    //        }
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                // Console.WriteLine("total count of lines  " + count);
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
 
-    //    }
+        }
     }
 }
